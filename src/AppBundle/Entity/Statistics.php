23f74d0707b36355561943456becae2bfe3f96a4
Javier Fernandez <javier.fdez@gmail.com>
2016-04-12 17:49:56 +0200

    Added Statistics entity

---
+++ src/AppBundle/Entity/Statistics.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\HasLifecycleCallbacks()
+ * @ORM\Table(name="statistics")
+ */
+class Statistics
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $type;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $count;
+}
